0444385adf423f23ad544c26f57dc03bdb5234a0
Macguire Rintoul <macguire.rintoul@gmail.com>
2018-11-23 22:17:04 -0800

    add lightbox to images

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {Component} from 'react'
 import styled from 'styled-components'
+import Lightbox from 'react-images'
 
 const StyledShowcase = styled.div`
+  .lightbox {
+    transition: 0.5s;
+  }
   div {
     min-width: 50%;
   }
@@ -17,6 +21,9 @@ const StyledShowcase = styled.div`
   img, video {
     max-height: 80vh;
   }
+  img {
+    cursor: pointer;
+  }
   align-items: center;
   margin: 2em 0;
   @media screen and (max-width: 600px) {
@@ -28,30 +35,57 @@ const StyledShowcase = styled.div`
   }
 `
 
-function Showcase(props) {
-  let media, first, second;
-  if (props.type == 'image') {
-    media = <img src={props.src} alt={props.alt}/>;
-  } else if (props.type == 'video') {
-    media = <video src={props.src} title={props.alt} autoPlay muted loop></video>
+class Showcase extends Component {
+  constructor () {
+		super();
+		this.state = {
+			lightboxIsOpen: false,
+		};
+
+		this.closeLightbox = this.closeLightbox.bind(this);
+		this.openLightbox = this.openLightbox.bind(this);
+	}
+  openLightbox (index, event) {
+		this.setState({
+			lightboxIsOpen: true,
+		});
+	}
+	closeLightbox () {
+		this.setState({
+			lightboxIsOpen: false,
+		});
+	}
+  render() {
+    let media, first, second;
+    if (this.props.type == 'image') {
+      media = <div><img src={this.props.src} alt={this.props.alt} onClick={this.openLightbox} /><Lightbox className="lightbox"
+          images={[{ src: this.props.src} ]}
+          isOpen={this.state.lightboxIsOpen}
+          onClose={this.closeLightbox}
+          backdropClosesModal
+          showImageCount={false}
+        /></div>;
+    } else if (this.props.type == 'video') {
+      media = <video src={this.props.src} title={this.props.alt} autoPlay muted loop></video>
+    }
+    if (this.props.mediaPosition == 'after') {
+      first = this.props.children;
+      second = media;
+    } else {
+      first = media;
+      second = this.props.children;
+    }
+    return(
+      <StyledShowcase {...this.props}>
+        <div>
+          {first}
+        </div>
+        <div>
+          {second}
+        </div>
+      </StyledShowcase>
+    );
   }
-  if (props.mediaPosition == 'after') {
-    first = props.children;
-    second = media;
-  } else {
-    first = media;
-    second = props.children;
-  }
-  return(
-    <StyledShowcase {...props}>
-      <div>
-        {first}
-      </div>
-      <div>
-        {second}
-      </div>
-    </StyledShowcase>
-  )
 }
 
 export default Showcase
