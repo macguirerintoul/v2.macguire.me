3d6a45a634557ffa4b5c3d02e58576339ca3d35a
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-01-13 14:26:15 -0800

    update grammar on the switch

--- src/pages/theswitch.js
+++ src/pages/theswitch.js
@@ -27,36 +27,38 @@ const TheSwitch = () => (
   <Layout>
     <Project>
       <h1>The Switch</h1>
-      <p>A web app introduction to menstrual cups that aims to address apprehensions and increase customer confidence. The four-week project is a hypothetical product for <a href='https://lenacup.com/' target='_blank'>Lena</a>, and is not affiliated with the brand.</p>
+      <p>An online introduction to menstrual cups aiming to address first-time user apprehensions and increase customer confidence. The four-week project is a hypothetical product for <a href='https://lenacup.com/' target='_blank'>Lena</a>, and is not affiliated with the brand.</p>
       <ProjectInformation client='4 week project for a senior design course' roles={['Content Strategy', 'Project Management']} tools={['Keynote', 'G Suite']} />
       <Showcase type='video' src={primaryVideo} alt='The primary information presented in The Switch' >
-        <p>The Switch has five steps that answer big questions and lessen frictions within the customer journey. Primary content is concise to ease visitors in, with more detailed information available below.</p>
+        <p>The Switch is comprised of five steps which answer major questions about menstrual cups, to reduce frictions within the customer journey. Content above the fold is concise to ease visitors in, with further details provided below.</p>
       </Showcase>
 
       <h2>Problem</h2>
-      <p>Despite the physical, environmental, and financial benefits of menstrual cups, lack of exposure in mainstream media has slowed their normalization. Furthermore, the majority of people that menstruate already use tampons, pads, or liners, and aren’t motivated to take the physical and emotional risk of switching products. While the Lena Cup is <a href="https://www.cosmopolitan.com/sex-love/a13126155/best-menstrual-cups/" target="_blank">recognized</a> as the <a href="https://www.bustle.com/p/the-5-best-menstrual-cups-for-beginners-8797916" target="_blank">#1 cup for beginners</a>, Lena's existing site does not effectively communicate the product's value and benefits.</p>
+      <p>Despite physical, environmental, and financial benefits of menstrual cups, lack of exposure in mainstream media and markets has prevented their widespread use. The majority of menstruators already use tampons, pads, and/or liners, and are not convinced to undertake the physical and emotional risk of switching menstrual hygiene products. While the Lena Cup is <a href="https://www.cosmopolitan.com/sex-love/a13126155/best-menstrual-cups/" target="_blank">recognized</a> as the <a href="https://www.bustle.com/p/the-5-best-menstrual-cups-for-beginners-8797916" target="_blank">#1 cup for beginners</a>, Lena's existing site does not effectively communicate the product's value and benefits to potential customers.</p>
 
       <div style={{padding:'56.25% 0 0 0', position:'relative'}}><iframe src="https://player.vimeo.com/video/301757528?title=0&byline=0&portrait=0" style={{position:'absolute',top:0,left:0, width:'100%', height: '100%'}} frameborder="0" webkitallowfullscreen mozallowfullscreen AllowFullScreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 
       <h2>Solution</h2>
-      <p>The Switch by Lena addresses false assumptions and apprehensions about menstrual cups head-on, to alleviate the concerns people have about trying one.</p>
+      <p>The Switch by Lena directly addresses false assumptions, apprehensions, and concerns consumers may have about menstrual cups.</p>
 
       <Showcase type='video' src={landingVideo} alt='The new landing page' >
-        <p>Leveraging the brand's #1 status and encouraging visitors to engage, The Switch acts as a hero element on Lena's landing page to capture newcomers.</p>
+        <p>Leveraging the brand's #1 status and encouraging visitor engagement, The Switch acts as a hero element on Lena's landing page to capture newcomers. Lena's existing home page content remains below.</p>
       </Showcase>
 
       <Showcase type='video' src={primaryVideo} alt='Primary information in The Switch' >
-        <p>The Switch aims to answer questions as soon as possible, so potential customers find what they're looking for.</p>
+        <p>The Switch aims to efficiently and clearly answer potential customers’ questions.</p>
       </Showcase>
 
       <Showcase type='video' src={secondaryVideo} alt='Secondary information in The Switch' >
-        <p>Each step of the onboarding has secondary information associated with it, to answer follow-up questions. To keep the experience authentic, the content was created and organized as if it were coming from a supportive older sister. Improved information design delivers details in a more understandable way.</p>
+        <p>Each step of the onboarding has secondary information associated with it to answer follow-up questions. </p>
       </Showcase>
+
+      <p>Our survey of over 450 respondents suggested that most customers were convinced to try a menstrual cup by a close friend or sibling. To create a meaningful experience, the team decided to strategize content from the perspective of a supportive, older sister. As the content strategist, I ensured that all content followed this principle and had a consistent tone and attitude.</p>
 
       <div style={{padding:'56.25% 0 0 0', position:'relative'}}><iframe src="https://player.vimeo.com/video/301758677?title=0&byline=0&portrait=0" style={{position:'absolute',top:0,left:0, width:'100%', height: '100%'}} frameborder="0" webkitallowfullscreen mozallowfullscreen AllowFullScreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 
       <h2>Process</h2>
-      <p>After identifying business problems in the menstrual cup market, we identified frictions with Lena's existing website. Information was distributed across multiple pages, and described the product without effectively explaining how it was better than alternatives. We then conducted a survey to understand peoples' perceptions of cups and suggest how they could be swayed.</p>
+      <p>After identifying business problems within the menstrual cup market, we identified frictions with Lena's existing website. Information was scattered across multiple pages, and neglected to highlight the product’s extensive benefits. We then conducted a survey to understand peoples' perception of cups and the factors influencing this perception, in order to design a digital experience that encouraged visitors to try the product.</p>
 
       <Showcase type='image' src={productApprehension} alt='Business Problem A: Product Apprehension' >
         <p>Business Problem A: Product Apprehension.</p>
@@ -66,19 +68,19 @@ const TheSwitch = () => (
         <p>Business Problem B: Established Habits.</p>
       </Showcase>
 
-      <p>While we acknowledged the various opportunities for the project, we focused on addressing apprehensions considering the scope and timeframe.</p>
+      <p>After considering the various opportunities for this project as well as its scope and timeframe, we focused on addressing the apprehensions of potential customers.</p>
 
       <Showcase type='image' src={opportunity} alt='Opportunity for the project' >
         <p>The opportunity: address apprehensions for those who have not used a menstrual cup before.</p>
       </Showcase>
 
-      <p>We then developed personas from common themes in our research. We chose to design for 'Accustomed & Uncertain', someone who uses other menstrual products and doesn't know enough about cups to switch over.</p>
+      <p>We then developed personas from common themes in our research: we chose to design for the 'Accustomed & Uncertain' menstruator, who uses other menstrual products and does not know enough about cups to switch over.</p>
 
       <Showcase type='image' src={persona} alt='Persona: Accustomed & Uncertain' >
         <p>One of the personas derived from our research: accustomed & uncertain.</p>
       </Showcase>
 
-      <p>The Lena customer journey is a bit rocky due to the nature of the product. The struggle of switching is recognized by the majority of cup users, but is said to be well worth it. Considering the business problems, we chose to address the pre-purchase stages and aimed to convey the value of menstrual cups more effectively.</p>
+      <p>The nature of the product lends itself to a rocky customer journey. Our research suggests the struggle of switching is recognized by the majority of cup users, but is said to be well worth it. Considering the business problems, we chose to address the pre-purchase stages and aimed to convey the value of menstrual cups more effectively.</p>
 
       <Showcase type='image' src={customerJourney} alt='The Lena customer journey' >
         <p>The customer journey map of someone purchasing a Lena Cup.</p>
@@ -107,7 +109,7 @@ const TheSwitch = () => (
         <p>Customers would then work with Lena to have their experience turned into a narrative that could help convince others to try a Lena Cup.</p>
       </Showcase>
       <h2>Reflection</h2>
-      <p>Throughout the project, we may have been too ambitious about the issues we were trying to address. Trying to reduce gender dysphoria with a brand that was fairly feminine was difficult, and reducing the stigma around periods could have been its own project. It was worthwhile to try, but deciding on one opportunity earlier in the project would have allowed more time for iteration and validation.</p>
+      <p>Throughout the project, we may have been too ambitious about the issues we were trying to address. Trying to reduce gender dysphoria with a brand that was fairly feminine was difficult, and reducing the stigma around periods could have been an entire project on its own. The team learned a lot trying to address these issues, but deciding on one opportunity earlier in the project would have allowed more time for iteration and validation.</p>
       <MoreProjects projects={['roar', 'winebox', 'leadrx', 'privileged']}/>
     </Project>
   </Layout>
