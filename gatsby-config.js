f614fce0538c7b8cdc3f8418b9096d4880a567cc
Macguire Rintoul <macguire.rintoul@gmail.com>
2019-01-14 20:47:26 -0800

    properly added babel styled components to see if it fixes anything

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    'babel-plugin-styled-components',
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
